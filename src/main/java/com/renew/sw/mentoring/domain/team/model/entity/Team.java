1e641041177855d7327ca5065b2e4a6ddea9ba9d
Jungwoo Kim <kjungw1025@naver.com>
2024-02-10 17:58:07 +0900

    feat: Team 엔티티 추가

---
+++ src/main/java/com/renew/sw/mentoring/domain/team/model/entity/Team.java
@@ -0,0 +1,40 @@
+package com.renew.sw.mentoring.domain.team.model.entity;
+
+import com.renew.sw.mentoring.domain.user.model.entity.User;
+import com.renew.sw.mentoring.global.base.BaseEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@RequiredArgsConstructor
+public class Team extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "team_id")
+    private Long id;
+
+    @NotBlank
+    private String teamName;
+
+    @NotNull
+    private int score;
+
+    @OneToMany(mappedBy = "team")
+    private List<User> users = new ArrayList<>();
+
+    @Builder
+    private Team(@NotNull String teamName,
+                @NotNull int score) {
+        this.teamName = teamName;
+        this.score = score;
+    }
+}
