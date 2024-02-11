73d88a37b32626519fc406b1078b1de0fe6b322f
ChaHyeonMin <gutanbug53@gmail.com>
2024-02-11 20:21:42 +0900

    feat: Redis를 사용할 때 설정할 Object 클래스 추가

---
+++ src/main/java/com/renew/sw/mentoring/global/model/CacheObject.java
@@ -0,0 +1,13 @@
+package com.renew.sw.mentoring.global.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.Instant;
+
+@Getter
+@RequiredArgsConstructor
+public class CacheObject<T> {
+    private final Instant expiresAt;
+    private final T value;
+}
