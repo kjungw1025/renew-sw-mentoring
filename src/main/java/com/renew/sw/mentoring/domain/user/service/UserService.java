d9dd0880860693a2737f43da7f4d33be91330f3d
Jungwoo Kim <kjungw1025@naver.com>
2024-02-12 15:01:44 +0900

    feat: 회원가입 시, 기본 랜덤 닉네임을 생성하는 로직 추가
    형용사 20개, 유명인 50명

--- src/main/java/com/renew/sw/mentoring/domain/user/service/UserService.java
+++ src/main/java/com/renew/sw/mentoring/domain/user/service/UserService.java
@@ -1,7 +1,13 @@
 package com.renew.sw.mentoring.domain.user.service;
 
 
+import com.renew.sw.mentoring.domain.user.exception.AlreadyNicknameException;
+import com.renew.sw.mentoring.domain.user.exception.UserNotFoundException;
+import com.renew.sw.mentoring.domain.user.exception.WrongPasswordException;
 import com.renew.sw.mentoring.domain.user.model.UserRole;
+import com.renew.sw.mentoring.domain.user.model.dto.request.RequestExistPasswordChangeDto;
+import com.renew.sw.mentoring.domain.user.model.dto.request.RequestLoginDto;
+import com.renew.sw.mentoring.domain.user.model.dto.request.RequestNickNameChangeDto;
 import com.renew.sw.mentoring.domain.user.model.dto.request.RequestSignupDto;
 import com.renew.sw.mentoring.domain.user.model.dto.response.ResponseLoginDto;
 import com.renew.sw.mentoring.domain.user.model.entity.User;
@@ -12,6 +18,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
@@ -24,12 +39,14 @@ public class UserService {
     private final JwtTokenProvider jwtProvider;
     private final PasswordEncoder passwordEncoder;
 
+    private final UserInfoService userInfoService;
+
     public void signup(RequestSignupDto dto) {
         User user = User.builder()
                 .studentId(dto.getStudentId())
                 .password(passwordEncoder.encode(dto.getPassword()))
                 .name(dto.getName())
-                .nickname(dto.getNickname())
+                .nickname(createRandomNickname())
                 .userRole(UserRole.MENTEE)
                 .build();
 
@@ -45,5 +62,51 @@ public class UserService {
         }
         AuthenticationToken token = jwtProvider.issue(user);
         return new ResponseLoginDto(token);
+    }
+
+    private String createRandomNickname() {
+        List<String> adjectives = new ArrayList<>();
+        List<String> celebrity = new ArrayList<>();
+
+        // 파일에서 형용사들을 읽어와 리스트에 저장
+        try (InputStream inputStream = getClass().getResourceAsStream("/adjectives.txt")) {
+            assert inputStream != null;
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    adjectives.add(line.trim());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 파일에서 유명인들을 읽어와 리스트에 저장
+        try (InputStream inputStream = getClass().getResourceAsStream("/celebrity.txt")) {
+            assert inputStream != null;
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    celebrity.add(line.trim());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 랜덤 객체 생성
+        Random random = new Random();
+
+        String result;
+        while(true) {
+            String randomAdjective = adjectives.get(random.nextInt(adjectives.size()));
+            String randomCelebrity = celebrity.get(random.nextInt(celebrity.size()));
+
+            result = randomAdjective + " " + randomCelebrity;
+
+            if (userRepository.findByNickname(result).isEmpty()) break;
+        }
+
+        return result;
     }
 }
