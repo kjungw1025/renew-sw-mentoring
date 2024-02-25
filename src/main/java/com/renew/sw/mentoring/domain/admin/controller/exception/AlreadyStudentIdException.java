3554c20441973c91c3e1f08da89e546410791798
ChaHyeonMin <gutanbug53@gmail.com>
2024-02-26 00:01:17 +0900

    feat #24 : 학번 존재 시 오류 클래스 추가

---
+++ src/main/java/com/renew/sw/mentoring/domain/admin/controller/exception/AlreadyStudentIdException.java
@@ -0,0 +1,11 @@
+package com.renew.sw.mentoring.domain.admin.controller.exception;
+
+import com.renew.sw.mentoring.global.error.exception.LocalizedMessageException;
+import org.springframework.http.HttpStatus;
+
+public class AlreadyStudentIdException extends LocalizedMessageException {
+
+    public AlreadyStudentIdException() {
+        super(HttpStatus.BAD_REQUEST, "already.student.id");
+    }
+}
