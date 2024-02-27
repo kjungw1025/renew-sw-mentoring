b01acdfffed74011b60acedf185202f4551d64b3
ChaHyeonMin <gutanbug53@gmail.com>
2024-02-28 00:04:51 +0900

    feat #31 : 미션, 보너스미션 레포지토리 추가

--- src/main/java/com/renew/sw/mentoring/domain/mission/repository/BonusMissionRepository.java
+++ src/main/java/com/renew/sw/mentoring/domain/mission/repository/BonusMissionRepository.java
@@ -2,6 +2,13 @@ package com.renew.sw.mentoring.domain.mission.repository;
 
 import com.renew.sw.mentoring.domain.mission.model.entity.BonusMission;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BonusMissionRepository extends JpaRepository<BonusMission, Long> {
+
+    @Query("select bm from BonusMission bm where bm.mission.id = :missionId")
+    List<BonusMission> findAllByMissionId(@Param("missionId") Long missionId);
 }
--- src/main/java/com/renew/sw/mentoring/domain/mission/repository/MissionRepository.java
+++ src/main/java/com/renew/sw/mentoring/domain/mission/repository/MissionRepository.java
@@ -1,7 +1,18 @@
 package com.renew.sw.mentoring.domain.mission.repository;
 
 import com.renew.sw.mentoring.domain.mission.model.entity.Mission;
+import org.springdoc.api.annotations.ParameterObject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
-public interface MissionRepository extends JpaRepository<Mission, Long> {
+public interface MissionRepository extends JpaRepository<Mission, Long>, JpaSpecificationExecutor<Mission> {
+
+    Page<Mission> findAll(Specification<Mission> spec, Pageable pageable);
+
+    @Query("select m from Mission m where m.id = (select bm.mission.id from BonusMission bm where bm.mission.id = m.id)")
+    Page<Mission> findAllWithBonusMission(Specification<Mission> spec, Pageable pageable);
 }
