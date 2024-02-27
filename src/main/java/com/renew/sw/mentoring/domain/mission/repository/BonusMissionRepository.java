774743374cce7f0c699221df91845b4f4c6c3559
ChaHyeonMin <gutanbug53@gmail.com>
2024-02-27 15:57:33 +0900

    feat: 보너스미션 레포지토리 추가

---
+++ src/main/java/com/renew/sw/mentoring/domain/mission/repository/BonusMissionRepository.java
@@ -0,0 +1,7 @@
+package com.renew.sw.mentoring.domain.mission.repository;
+
+import com.renew.sw.mentoring.domain.mission.model.entity.BonusMission;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BonusMissionRepository extends JpaRepository<BonusMission, Long> {
+}
