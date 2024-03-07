ce62e4abc8558a8c7c687b2b7ca0c569bec73f0c
ChaHyeonMin <gutanbug53@gmail.com>
2024-03-07 10:34:10 +0900

    feat #33 : NoticeRepository 추가

---
+++ src/main/java/com/renew/sw/mentoring/domain/post/repository/NoticeRepository.java
@@ -0,0 +1,7 @@
+package com.renew.sw.mentoring.domain.post.repository;
+
+import com.renew.sw.mentoring.domain.post.model.entity.type.Notice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeRepository extends GenericPostRepository<Notice>{
+}
