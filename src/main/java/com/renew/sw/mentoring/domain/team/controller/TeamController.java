c8d6212c9c95ca570910b6c1f22ccda31773be42
Jungwoo Kim <kjungw1025@naver.com>
2024-03-11 21:38:54 +0900

    feat: 팀 컨트롤러에 성공한 미션 상세 조회 기능 추가

--- src/main/java/com/renew/sw/mentoring/domain/team/controller/TeamController.java
+++ src/main/java/com/renew/sw/mentoring/domain/team/controller/TeamController.java
@@ -1,5 +1,7 @@
 package com.renew.sw.mentoring.domain.team.controller;
 
+import com.renew.sw.mentoring.domain.completedmission.service.CompletedMissionService;
+import com.renew.sw.mentoring.domain.mission.model.dto.list.SummarizedMissionDto;
 import com.renew.sw.mentoring.domain.team.model.dto.list.SummarizedTeamDto;
 import com.renew.sw.mentoring.domain.team.model.dto.response.ResponsePage;
 import com.renew.sw.mentoring.domain.team.model.dto.response.ResponseTeamInfoDto;
@@ -8,6 +10,7 @@ import com.renew.sw.mentoring.global.auth.jwt.AppAuthentication;
 import com.renew.sw.mentoring.global.auth.role.UserAuth;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.data.domain.Page;
@@ -21,6 +24,7 @@ import org.springframework.data.domain.Pageable;
 public class TeamController {
 
     private final TeamService teamService;
+    private final CompletedMissionService completedMissionService;
 
     /**
      * 전체 팀 조회
@@ -44,4 +48,26 @@ public class TeamController {
     public ResponseTeamInfoDto myTeam(AppAuthentication auth) {
         return teamService.getMyTeamInfo(auth.getUserId());
     }
+
+    /**
+     * 팀별 성공한 미션 상세 조회
+     */
+    @GetMapping("/success/mission/{teamId}")
+    public ResponsePage<SummarizedMissionDto> list(@ParameterObject Pageable pageable,
+                                                   @PathVariable Long teamId) {
+        Page<SummarizedMissionDto> result = completedMissionService.list(pageable, teamId);
+        return new ResponsePage<>(result);
+    }
+
+    /**
+     * 우리 팀의 성공한 미션 상세 조회
+     */
+    @GetMapping("/success/mission")
+    @UserAuth
+    public ResponsePage<SummarizedMissionDto> listMyTeam(AppAuthentication auth,
+                                                         @ParameterObject Pageable pageable) {
+        Page<SummarizedMissionDto> result = completedMissionService.listMyTeam(pageable, auth.getUserId());
+        return new ResponsePage<>(result);
+    }
+
 }
