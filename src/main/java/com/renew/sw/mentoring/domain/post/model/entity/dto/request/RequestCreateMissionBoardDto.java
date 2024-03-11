56c72e1f68cd481c02f0899511b108407cebfe57
Jungwoo Kim <kjungw1025@naver.com>
2024-03-11 21:03:41 +0900

    feat: 미션 게시글 관련 dto 추가

---
+++ src/main/java/com/renew/sw/mentoring/domain/post/model/entity/dto/request/RequestCreateMissionBoardDto.java
@@ -0,0 +1,38 @@
+package com.renew.sw.mentoring.domain.post.model.entity.dto.request;
+
+import com.renew.sw.mentoring.domain.post.model.entity.RegisterStatus;
+import com.renew.sw.mentoring.domain.post.model.entity.type.MissionBoard;
+import com.renew.sw.mentoring.domain.user.model.entity.User;
+import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@Getter
+public class RequestCreateMissionBoardDto extends RequestCreateGenericPostDto<MissionBoard> {
+
+    private final Long missionId;
+    private final Boolean isBonusMissionSuccessful;
+    public RequestCreateMissionBoardDto(@NotBlank String title,
+                                        @NotBlank String body,
+                                        @NotBlank Long missionId,
+                                        Boolean isBonusMissionSuccessful,
+                                        List<MultipartFile> images,
+                                        List<MultipartFile> files) {
+        super(title, body, images, files);
+        this.missionId = missionId;
+        this.isBonusMissionSuccessful = isBonusMissionSuccessful;
+    }
+
+    public MissionBoard toEntity(User user) {
+        return MissionBoard.builder()
+                .user(user)
+                .title(getTitle())
+                .body(getBody())
+                .missionId(missionId)
+                .isBonusMissionSuccessful(isBonusMissionSuccessful != null && isBonusMissionSuccessful)
+                .registerStatus(RegisterStatus.IN_PROGRESS)
+                .build();
+    }
+}
