83b4b58ac0a3e417ceaabe7d0ed84f985694a847
ChaHyeonMin <gutanbug53@gmail.com>
2024-03-22 16:29:34 +0900

    feat : 미션 인증 글과 공지사항 글에 댓글 및 대댓글 기능 추가

--- src/main/java/com/renew/sw/mentoring/domain/post/controller/NoticeController.java
+++ src/main/java/com/renew/sw/mentoring/domain/post/controller/NoticeController.java
@@ -1,5 +1,8 @@
 package com.renew.sw.mentoring.domain.post.controller;
 
+import com.renew.sw.mentoring.domain.comment.CommentService;
+import com.renew.sw.mentoring.domain.comment.model.dto.request.RequestCreateCommentDto;
+import com.renew.sw.mentoring.domain.comment.model.dto.response.SummarizedCommentDto;
 import com.renew.sw.mentoring.domain.post.model.entity.dto.list.SummarizedGenericPostDto;
 import com.renew.sw.mentoring.domain.post.model.entity.dto.request.RequestCreateNoticeDto;
 import com.renew.sw.mentoring.domain.post.model.entity.dto.request.RequestUpdateNoticeDto;
@@ -19,6 +22,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.List;
+
 @RestController
 @Slf4j
 @RequiredArgsConstructor
@@ -27,6 +33,7 @@ import org.springframework.web.bind.annotation.*;
 public class NoticeController {
 
     private final NoticeService noticeService;
+    private final CommentService commentService;
 
     /**
      * 공지사항 목록 조회
@@ -92,4 +99,60 @@ public class NoticeController {
         noticeService.delete(auth.getUserId(), id, auth.getUserRole());
     }
 
+    /**
+     * 모든 댓글을 조회합니다.
+     */
+    @GetMapping("/comment/{postId}")
+    public List<SummarizedCommentDto> listComments(@PathVariable Long postId) {
+        return commentService.list(postId);
+    }
+
+    /**
+     * 게시글에 댓글을 생성합니다.
+     **/
+    @PostMapping("/comment/{postId}")
+    @UserAuth
+    public ResponseIdDto createComment(AppAuthentication auth,
+                                @PathVariable Long postId,
+                                @Valid @RequestBody RequestCreateCommentDto dto) {
+        Long result = commentService.create(postId, auth.getUserId(), dto.getContent());
+        return new ResponseIdDto(result);
+    }
+
+    /**
+     * 댓글을 수정합니다.
+     * <p>대댓글도 수정할 수 있습니다.</p>
+     */
+    @PatchMapping("/comment/{commentId}")
+    @UserAuth
+    public void editComment(AppAuthentication auth,
+                     @PathVariable Long commentId,
+                     @Valid @RequestBody RequestCreateCommentDto dto) {
+        commentService.edit(commentId, auth.getUserId(), dto.getContent());
+    }
+
+    /**
+     * 댓글을 삭제합니다.
+     * <p>대댓글도 삭제할 수 있습니다.</p>
+     */
+    @DeleteMapping("/comment/{commentId}")
+    @UserAuth
+    public void deleteComment(AppAuthentication auth,
+                       @PathVariable Long commentId) {
+        commentService.delete(commentId, auth.getUserId());
+    }
+
+    /**
+     * 대댓글을 생성합니다.
+     *
+     * @param commentId   댓글 ID
+     */
+    @PostMapping("/reply/{commentId}")
+    @UserAuth
+    public ResponseIdDto createReply(AppAuthentication auth,
+                                     @PathVariable Long commentId,
+                                     @Valid @RequestBody RequestCreateCommentDto dto) {
+        Long result = commentService.createReply(commentId, auth.getUserId(), dto.getContent());
+        return new ResponseIdDto(result);
+    }
 }
