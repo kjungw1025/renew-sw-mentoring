b1a90e80a58b634fb3ac2a95b99b163ffe2fa4f4
ChaHyeonMin <gutanbug53@gmail.com>
2024-04-08 17:22:09 +0900

    fix : 미션인증글 레포지토리 Query 수정

--- src/main/java/com/renew/sw/mentoring/domain/post/repository/MissionBoardRepository.java
+++ src/main/java/com/renew/sw/mentoring/domain/post/repository/MissionBoardRepository.java
@@ -6,10 +6,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface MissionBoardRepository extends GenericPostRepository<MissionBoard> {
-    @Query("select m from MissionBoard m where m.user.id = :userId and m.registerStatus = 'IN_PROGRESS' ")
+    @Query("select m from MissionBoard m where m.user.id = :userId and (m.registerStatus = 'IN_PROGRESS' or m.registerStatus = 'ACCEPTED') order by m.createdAt desc ")
     Page<MissionBoard> findAllByUserId(@Param("userId") Long userId, Pageable pageable);
 
-    @Query("select m from MissionBoard m where m.registerStatus = 'IN_PROGRESS' ")
+    @Query("select m from MissionBoard m where m.registerStatus = 'IN_PROGRESS' order by m.createdAt asc ")
     Page<MissionBoard> findAllProgressPosts(Pageable pageable);
+
+    @Query("select m from MissionBoard m where m.registerStatus = 'IN_PROGRESS' and m.user.id = :userId and m.missionId = :missionId")
+    Optional<MissionBoard> findByMissionIdAndUserId(@Param("missionId") Long missionId, @Param("userId") Long userId);
 }
