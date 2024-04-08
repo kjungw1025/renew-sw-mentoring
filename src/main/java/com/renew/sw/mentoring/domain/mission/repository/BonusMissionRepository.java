f1faf51ea182c0b4ea1c25ee8a743aadb7cd8906
ChaHyeonMin <gutanbug53@gmail.com>
2024-04-09 00:14:49 +0900

    fix : 미션 아이디로 보너스 미션 조회할 때 없으면 에러나게끔 수정

--- src/main/java/com/renew/sw/mentoring/domain/mission/repository/BonusMissionRepository.java
+++ src/main/java/com/renew/sw/mentoring/domain/mission/repository/BonusMissionRepository.java
@@ -6,11 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BonusMissionRepository extends JpaRepository<BonusMission, Long> {
 
     @Query("select bm from BonusMission bm where bm.mission.id = :missionId")
     List<BonusMission> findAllByMissionId(@Param("missionId") Long missionId);
 
-
+    @Query("select bm from BonusMission bm where bm.mission.id = :missionId ")
+    Optional<BonusMission> findByMissionId(@Param("missionId") Long missionId);
 }
